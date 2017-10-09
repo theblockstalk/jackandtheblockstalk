5347d642228618507df790463a278de59f53414a
Jack Tanner <ugmo04@users.noreply.github.com>
2017-10-09 23:59:40 +0100

    Added page metadata images, description etc

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 
 const BLOCK1_BODY = "<h4><a href='mailto:jack@jackandtheblockstalk.com'>jack@jackandtheblockstalk.com</a></h4>\
-  <h4>Ethereum: <a href='https://etherscan.io/enslookup?q=jackandtheblockstalk.eth- target='blank'>jackandtheblockstalk.eth</a></h4>\
+  <h4>Ethereum: <a href='https://etherscan.io/enslookup?q=jackandtheblockstalk.eth' target='blank'>jackandtheblockstalk.eth</a></h4>\
   <div align='center'>\
     <div class='row'>\
       <div class='col-md-4'>\
@@ -32,8 +32,8 @@ const BLOCK1_BODY = "<h4><a href='mailto:jack@jackandtheblockstalk.com'>jack@jac
   <!-- <h4><br></h4> -->\
   <h6><br></h6>";
 
-const BLOCK2_BODY = "<h4>Coming very soon...</h4>\
-  <h4><br></h4>\
+const BLOCK2_BODY = "<h4>Senior Software Engineer, <a href='https://indorse.io/' target='blank'>Indorse</a></h4>\
+  <h4>Colaborator, <a href='https://www.theblockchainconnector.com/' target='blank'>The Blockchain Connector</a></h4>\
   <h4><br></h4>\
   <h4><br></h4>\
   <h4><br></h4>\
@@ -65,70 +65,70 @@ const blockdata = [
   },
 ];
 
-const VALID_COMPARE = "00";
+const VALID_COMPARE = '00';
 
 function nextNonce(_nonce, _i) {
-  // console.log("Trying nonce " + _nonce + " for block " + _i);
-  var jname = "#" + blockdata[_i].name;
+  // console.log('Trying nonce ' + _nonce + ' for block ' + _i);
+  var jname = '#' + blockdata[_i].name;
 
   // Try a new hash and check if the first few characters are 0's
   var blockHash = Sha256.hash(blockdata[_i].name + _nonce);
   var validString = blockHash.substr(0,VALID_COMPARE.length);
   var totalString = blockHash.substr(0,10);
-  $(jname + " .nonce").text(_nonce);
-  $(jname + " .final-hash").text(totalString);
+  $(jname + ' .nonce').text(_nonce);
+  $(jname + ' .final-hash').text(totalString);
 
   if( validString != VALID_COMPARE && _nonce < 1000000 ) {
-    // Correct hash not found, "block not valid"
+    // Correct hash not found, 'block not valid'
     newNonce = _nonce+1;
     setTimeout( function() { nextNonce(newNonce, _i); }, 1)
   }
   else {
-    console.log("Finished mining block " + _i);
+    console.log('Finished mining block ' + _i);
     makeBlock(_i+1);
   }
 }
 
 function makeBlock(i) {
-  console.log("Making block " + i);
+  console.log('Making block ' + i);
   // If there are no more blocks then finish
   if (i < blockdata.length) {
-    var jname = "#" + blockdata[i].name;
+    var jname = '#' + blockdata[i].name;
 
     // Fade in the block
     $(jname).fadeIn('slow', () => {
 
       // Fade in the nonce, previous and final has
-      $.when($(jname + " .block-title-box, " + jname + " .block-final-box").fadeOut(0)).done( () => {
-      // $(jname + " .block-title-box, " + jname + " .block-final-box").fadeOut(0, () => {
+      $.when($(jname + ' .block-title-box, ' + jname + ' .block-final-box').fadeOut(0)).done( () => {
+      // $(jname + ' .block-title-box, ' + jname + ' .block-final-box').fadeOut(0, () => {
         // Set the title box with 0 nonce and previous hash
         var titleData;
         if (i == 0)
           titleData = "<div class='box-title'><h2 align='center' class='box-title'><br></h2></div><span class='light-text'>Previous Hash: <span class='prev-hash'>" + PREVIOUS_HASH_0 + "</span><br>Nonce: <span class='nonce'>0</span><span>";
         else {
-          var prevHash = $(jname + " .final-hash").text();
+          var prevHash = $(jname + ' .final-hash').text();
           titleData = "<div class='box-title'><h2 align='center' class='box-title'><br></h2></div><span class='light-text'>Previous Hash: <span class='prev-hash'>" + prevHash + "</span><br>Nonce: <span class='nonce'>0</span><span>";
         }
-        $(jname + " .block-title-box").html(titleData);
+        $(jname + ' .block-title-box').html(titleData);
         // Set the final box with dummy final hash
         var finalData = "<span class='light-text'>Hash: <span class='final-hash'>ffffffffff</span></span>";
-        $(jname + " .block-final-box").html(finalData);
+        $(jname + ' .block-final-box').html(finalData);
 
-        $.when($(jname + " .block-title-box, " + jname + " .block-final-box").fadeIn('slow')).done( () => {
-        // $(jname + " .block-title-box, " + jname + " .block-final-box").fadeIn(2000, () => {
+        $.when($(jname + ' .block-title-box, ' + jname + ' .block-final-box').fadeIn('slow')).done( () => {
+        // $(jname + ' .block-title-box, ' + jname + ' .block-final-box').fadeIn(2000, () => {
           // Fade in the title and the body
           $.when($(jname + ' .block-title-box .box-title, ' + jname + ' .block-body-box').fadeOut(0)).done( () => {
           // $(jname + ' .block-title-box .box-title, ' + jname + ' .block-body-box').fadeOut(0, () => {
             // Set the title box with title
             var titleData = "<h2 align='center'>" + blockdata[i].title + "</h2>";
-            $(jname + " .block-title-box .box-title").html(titleData);
+            $(jname + ' .block-title-box .box-title').html(titleData);
             // Set the body box with body html
-            $(jname + " .block-body-box").html(blockdata[i].body);
+            $(jname + ' .block-body-box').html(blockdata[i].body);
 
-            $.when($(jname + " .block-title-box .box-title, " + jname + " .block-body-box").fadeIn('slow')).done( () => {
-            // $(jname + " .block-title-box .box-title, " + jname + " .block-body-box").fadeIn(2000, () => {
+            $.when($(jname + ' .block-title-box .box-title, ' + jname + ' .block-body-box').fadeIn('slow')).done( () => {
+            // $(jname + ' .block-title-box .box-title, ' + jname + ' .block-body-box').fadeIn(2000, () => {
               // Go through nonces and check if final hash is valid
-              console.log("Starting mine block " + i);
+              console.log('Starting mine block ' + i);
               nextNonce(0, i);
               // makeBlock(i+1);
             })
@@ -145,30 +145,30 @@ function blockchain() {
 }
 
 function blockchain_old() {
-  var jname = "#block0";
+  var jname = '#block0';
   // Fade in the block
-  $(jname).fadeIn("slow", () => {
+  $(jname).fadeIn('slow', () => {
 
     // Fade in the nonce, previous and final has
-    $(jname + " .block-title-box, " + jname + " .block-final-box").fadeOut(0, () => {
+    $(jname + ' .block-title-box, ' + jname + ' .block-final-box').fadeOut(0, () => {
       // Set the title box with 0 nonce and previous hash
       var titleData = "<div class='box-title'><h2 align='center' class='box-title'><br></h2></div><span class='light-text'>Previous Hash: <span class='prev-hash'>" + PREVIOUS_HASH_1 + "</span><br>Nonce: <span class='nonce'>0</span><span>";
-      $(jname + " .block-title-box").html(titleData);
+      $(jname + ' .block-title-box').html(titleData);
       // Set the final box with dummy final hash
       var finalData = "<span class='light-text'>Hash: <span class='final-hash'>ffffffffff</span></span>";
-      $(jname + " .block-final-box").html(finalData);
+      $(jname + ' .block-final-box').html(finalData);
 
-      $(jname + " .block-title-box, " + jname + " .block-final-box").fadeIn("slow", () => {
+      $(jname + ' .block-title-box, ' + jname + ' .block-final-box').fadeIn('slow', () => {
 
         // Fade in the title and the body
         $(jname + ' .block-title-box .box-title, ' + jname + ' .block-body-box').fadeOut(0, () => {
           // Set the title box with title
           var titleData = "<h2 align='center'>" + blockdata[0].title + "</h2>";
-          $(jname + " .block-title-box .box-title").html(titleData);
+          $(jname + ' .block-title-box .box-title').html(titleData);
           // Set the body box with body html
-          $(jname + " .block-body-box").html(blockdata[0].body);
+          $(jname + ' .block-body-box').html(blockdata[0].body);
 
-          $(jname + " .block-title-box .box-title, " + jname + " .block-body-box").fadeIn("slow", () => {
+          $(jname + ' .block-title-box .box-title, ' + jname + ' .block-body-box').fadeIn('slow', () => {
 
             // Go through nonces and check if final hash is valid
             nextNonce(0, 0, jname, blockdata[0].name);
